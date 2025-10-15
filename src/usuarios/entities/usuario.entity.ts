a47ba257599d9f479b9f765ce7e367e696b484af
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-10-14 20:52:39 -0700

    Creación del crud usuario y cambios en el schema de la BD.

---
+++ src/usuarios/entities/usuario.entity.ts
@@ -0,0 +1,15 @@
+export class Usuario {
+    id: number;
+    nombre: string;
+    apellidoP: string;
+    apellidoM: string;
+    correo: string;
+    contrasena: string;
+    fechaCreacion: Date;
+    fotoPerfil: string;
+    estatus: string;
+    presupuesto: number;
+    ingresoMinimo: number;
+    egresoMaximo: number;
+    ahorroMensual: number;
+};
