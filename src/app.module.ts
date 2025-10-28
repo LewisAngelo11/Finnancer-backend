bcd18da5e33b366b18f72a223adf161183ac7522
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-10-28 15:27:15 -0700

    Se creó el CRUD de perfiles.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { VerificationModule } from './verification/verification.module';
+import { PerfilesModule } from './perfiles/perfiles.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsuariosModule, AuthModule, VerificationModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, UsuariosModule, AuthModule, VerificationModule, PerfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
