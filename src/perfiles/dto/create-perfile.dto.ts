a81257c2932ba4093a5d73d8c4e77487211d7652
LewisAngelo11 <luisangel.sotog11@gmail.com>
2025-10-28 15:43:18 -0700

    Se definió el DTO de perfiles y la entidad perfil.

--- src/perfiles/dto/create-perfile.dto.ts
+++ src/perfiles/dto/create-perfile.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePerfileDto {}
+import { IsNumber, IsString, IsDate } from 'class-validator';
+
+export class CreatePerfileDto {
+    @IsString()
+    nombre: string;
+
+    @IsNumber()
+    icono: number;
+
+    @IsDate()
+    fechaCreacion: Date;
+}
--- src/perfiles/entities/perfile.entity.ts
+++ src/perfiles/entities/perfile.entity.ts
@@ -1,1 +1,7 @@
-export class Perfile {}
+export class Perfile {
+    id: number;
+    nombre: string;
+    icono: number;
+    fechaCreacion: Date;
+    estatus: string;
+}
